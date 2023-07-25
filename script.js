407b568f4fe4ef622ed7e5817cf67f2f2ad7a954
kajal-1999-cloud <130256390+kajal-1999-cloud@users.noreply.github.com>
2023-07-25 11:13:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your JS code here. If required.
+let input = document.getElementsById('code');
+
+	for(let i=0; i< input.length; i++){
+		input[i].addEventListener('keyup', (event) =>{
+	  // 
+			// it will target the current html input at ith
+			let currentElement = event.target ;
+          if(event.key === "Backspace"){
+			  // we will target the previous element
+			  let prevElement = currentElement.previousElementSibling;
+			  //precaution for first input -- we will ensure the current input is not first as there will no prevElement 
+				 if(prevElement){
+					 prevElement.focus();
+				 }
+		  }
+			// check the input is digit or not
+			let code = event.key.charCodeAt(0);
+			if(48 <= code && code<=57) {
+				// if input is num we will target next element
+			let nextElement  = currentElement.nextElementSibling;
+				if(nextElement) {
+					nextElement.focus();
+				}
+				else{
+					console.log(event.target.value);
+					event.target.value = "";
+				}
+			}
+			
+		}
+})
